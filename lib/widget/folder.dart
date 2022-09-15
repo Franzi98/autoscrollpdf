41186ca0c125951fade38c5f4df0fa203e7ddb27
Franzix98 <valeplay.98@gmail.com>
2022-09-15 20:29:33 +0200

    add Folder widget

---
+++ lib/widget/folder.dart
@@ -0,0 +1,27 @@
+import 'package:autoscrollpdf/utily/tags.dart';
+import 'package:flutter/material.dart';
+
+class FolderCard extends StatelessWidget {
+  Image img;
+  TAGS tag;
+  FolderCard({Key? key, required this.img, required this.tag})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: const RoundedRectangleBorder(
+          side: BorderSide(width: 3),
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: InkWell(
+        //onTap: () => Navigator.of(context).push(),
+        child: Container(
+          width: MediaQuery.of(context).size.width / 4,
+          height: MediaQuery.of(context).size.height / 7,
+          child: Image(image: img.image),
+        ),
+      ),
+    );
+  }
+}
