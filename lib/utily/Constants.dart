1256f6b4f8ec1a3bb73105b8a75db16bc96eb2e1
Franzix98 <valeplay.98@gmail.com>
2022-09-15 20:29:20 +0200

    add some constants

--- lib/utily/Constants.dart
+++ lib/utily/Constants.dart
@@ -1,3 +1,7 @@
 class Constants {
-  static String version = "0.2 alpha test";
+  static String version = "0.3 alpha test";
+
+  static String duration = "Lentezza Scorrimento";
+  static String offset = "Offset";
+  static String tags = "Tags";
 }
