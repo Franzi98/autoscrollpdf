664f3814aac8e533ce6914d09a15a0fe49465f71
Franzix98 <valeplay.98@gmail.com>
2022-10-18 12:09:39 +0200

    add tagToString function

--- lib/Helper/Functions.dart
+++ lib/Helper/Functions.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:autoscrollpdf/utily/tags.dart';
+
 class Functions {
   /*
   input: link
@@ -37,5 +39,21 @@ class Functions {
   static String makeTitle(String path) {
     List<String> list = path.split('/');
     return list.last;
+  }
+
+  /*
+  input: TAGS
+  output: tag as String
+  */
+  static tagToString(TAGS tag) {
+    return tag.toString().split('.').last;
+  }
+
+  static listTagsToListString(List<TAGS> tags) {
+    List<String> res = [];
+    tags.forEach((element) {
+      res.add(element.toString().split('.').last);
+    });
+    return res;
   }
 }
