7eea7575a9181af2edba1aca7328ce9a0490efaf
Franzix98 <valeplay.98@gmail.com>
2022-10-18 12:09:52 +0200

    add 3 colors to constants class

--- lib/utily/Constants.dart
+++ lib/utily/Constants.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static String version = "0.3 alpha test";
 
   static String duration = "Lentezza Scorrimento";
   static String offset = "Offset";
   static String tags = "Tags";
+
+  static const Color primaryColor = Color.fromARGB(64, 120, 188, 255);
+  static const Color secondaryColor = Color.fromARGB(255, 170, 222, 255);
+  static const Color alternativeColor = Color.fromARGB(255, 255, 255, 255);
 }
